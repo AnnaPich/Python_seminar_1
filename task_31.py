3fe8da95c584faa3da792ac8fb96c73446bab7fc
Anna <anek_ogonek@mail.ru>
2023-03-12 14:37:45 +0300

    added a task_31

---
+++ task_31.py
@@ -0,0 +1,17 @@
+# Задача 31.
+# Последовательностью Фибоначчи называется последовательность чисел a0, a1, ..., an, ..., где
+# a0 = 0, a1 = 1, ak = ak-1 + ak-2 (k > 1).
+#
+# Требуется найти N-е число Фибоначчи
+
+def fib(n):
+    if n in [1, 2]:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+numb = int(input("Enter number: "))
+list_1 = []
+for i in range(1, numb + 1):
+    list_1.append(fib(i))
+print(list_1)  # [1, 1, 2, 3, 5, 8, 13, 21, 34]
